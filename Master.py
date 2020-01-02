8fd830a0b27f854d8286e323141a19b123e45553
Yuri Potawsky <potawsky.yuri@gmail.com>
2020-01-02 14:52:06 -0500

    laspy

--- Master.py
+++ Master.py
@@ -1,7 +1,11 @@
-def bike(color):
-    print("Bike Compeleted")
-    print(color)
+import pylas
+import laspy
+import numpy as np
 
-bike("red")
 
+
+
+las = pylas.read("C:\\Users\\yuri7100\\Desktop\\lidar_raleigh_nc_spm_height_feet_las\\lidar_raleigh_nc_spm_height_feet.las")
+print(las.header)
+print(las.z.max())
 
