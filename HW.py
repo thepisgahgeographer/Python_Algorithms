358294a42e0f61ee8a0e6254485af1a51520e353
Yuri Potawsky <potawsky.yuri@gmail.com>
2020-01-09 16:25:32 -0500

    Exercises

--- HW.py
+++ HW.py
@@ -3,3 +3,14 @@ def main():
 
 main()
 
+
+Name = input("Print Name")
+age = 35
+
+year = 100 - age
+final = year + 2020
+
+#print(year)
+#print(final)
+
+print(Name + " you will turn 100 in " + str(final))
