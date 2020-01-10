3db814b125a85b3d61dcca08915abf78d6d3fb9d
Yuri Potawsky <potawsky.yuri@gmail.com>
2020-01-10 13:21:51 -0500

    New

--- HW.py
+++ HW.py
@@ -4,7 +4,7 @@ def main():
 main()
 
 
-Name = input("Print Name")
+#Name = input("Print Name")
 age = 35
 
 year = 100 - age
@@ -13,4 +13,19 @@ final = year + 2020
 #print(year)
 #print(final)
 
-print(Name + " you will turn 100 in " + str(final))
+#print(Name + " you will turn 100 in " + str(final))
+
+
+number = 4
+test = number % 2
+
+if test > 0:
+    print("Odd")
+elif number / 4:
+    print("Four")
+#else:
+#   print("Even")
+
+
+a = ["Ragley", "Piglet", 165]
+print(a[2])
