6566147be11a01f6c78bc41af9ff36f6756224c7
Yuri Potawsky <potawsky.yuri@gmail.com>
2020-07-29 14:55:29 -0400

    added_Crash

--- Tkinter_GUI.py
+++ Tkinter_GUI.py
@@ -3,12 +3,16 @@
 from tkinter import * 
 from tkinter import ttk 
 from tkinter.filedialog import asksaveasfile 
+from tkinter.filedialog import askopenfile 
+import laspy
+import numpy
   
 root = Tk() 
 root.geometry('700x250') 
 
 def open_file(): 
-    file = askopenfile(mode ='r', filetypes =[('Python Files', '*.py')]) 
+    global infile
+    infile = askopenfile(mode ='r', filetypes =[('Raw LAS Files', '.las')]) 
     if file is not None: 
         content = file.read() 
         print(content) 
@@ -18,6 +22,14 @@ def save():
              ('Python Files', '*.py'), 
              ('Text Document', '*.txt')] 
     file = asksaveasfile(filetypes = files, defaultextension = files) 
+
+def processLAS():
+    lasFile =  File(infile, mode = "r")
+    headerformat = lasFile.header.header_format
+    for spec in headerformat:
+        print(spec.name)
+
+
   
 btn1 = ttk.Button(root, text = 'Save File', command = lambda : save()) 
 btn1.pack(side = BOTTOM, pady = 20) 
